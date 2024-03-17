b94f25522e2cd83d3a648d3803906dfb3824613a
rivalcoba <ivan.rivalcoba@gmail.com>
2024-03-16 21:24:38 -0600

    ✨ FEAT: Componentes anidados

--- src/components/plan-picker.vue
+++ src/components/plan-picker.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="plans">
-    <plan-item v-for="plan in plans" :key="plan" :name="plan" />
+    <plan-picker-item v-for="plan in plans" :key="plan" :name="plan" />
   </div>
 </template>
 
 <script setup>
-  import planItem from './plan-item.vue';
+  import planPickerItem from './plan-picker-item.vue';
   import {ref} from 'vue';
   const plans = ref(['El soltero', 'El curioso', 'El adicto']);
 </script>
