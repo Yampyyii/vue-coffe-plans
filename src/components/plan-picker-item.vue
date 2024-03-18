3997306c66564891caf16933c7e1033246338e3f
rivalcoba <ivan.rivalcoba@gmail.com>
2024-03-17 18:52:46 -0600

    ⭐ FEAT: Selección de un plan

--- src/components/plan-picker-item.vue
+++ src/components/plan-picker-item.vue
@@ -1,14 +1,22 @@
 <script setup>
+import { ref } from 'vue';
 defineProps({
   name: String
 });
+//
+const selected = ref(false);
+const select = () => {
+  selected.value = true;
+};
 </script>
 
 <template>
-  <div class="plan">
+  <div 
+    @click="select" 
+    class="plan" :class="{'selected-plan': selected}">
     <div class="description">
       <span class="title">
-        {{name}}
+        {{name}} {{ selected ? '👍' : '' }}
       </span>
     </div>
   </div>
