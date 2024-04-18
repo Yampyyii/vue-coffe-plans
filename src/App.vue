4c9e4431ecac6051cec0f22bf373e1f94f645d30
rivalcoba <ivan.rivalcoba@gmail.com>
2024-04-18 12:43:28 -0500

    🌠 Nested Components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <script setup>
-import { ref } from 'vue';
-import planItem from './components/plan-item.vue';
-const plans = ref([
-  "El soltero",
-  "El adicto",
-  "El viajero",
-  "El Colombiano",
-  "El Italiano",]);
+import planPicker from './components/plan-picker.vue';
 </script>
 
 <template>
@@ -19,17 +12,13 @@ const plans = ref([
 
   <div class="content">
     <h1 class="title">Coffee Plans</h1>
-
     <h2 class="subtitle">
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
     </h2>
 
-    <div class="plans">
-      <plan-item 
-        v-for="plan in plans" 
-        :name="plan" 
-        v-bind:key="plan"/>
-    </div>
+    <plan-picker />
+
+    
   </div>
 </template>
 
---
+++ src/components/plan-picker.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="plans">
+    <plan-picker-item 
+      v-for="plan in plans" 
+      :name="plan" 
+      v-bind:key="plan" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import planPickerItem from './plan-picker-item.vue';
+const plans = ref([
+  "El soltero",
+  "El adicto",
+  "El viajero",
+  "El Colombiano",
+  "El Italiano",]);
+</script>
