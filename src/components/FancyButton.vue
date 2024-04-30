3801841814ff6797e533a552964ee107ce972763
Pallares <ing.pallares69@gmail.com>
2024-04-30 12:52:37 -0600

    😘slots

---
+++ src/components/FancyButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+<template>
+<div class="content">
+    <button>Haz Clic😘</button>
+    </div>
+</template>
+<style scoped>
+.content{
+  text-align: center;
+}
+button{
+  width: 10rem;
+margin: 30px auto; 
+font-size: 2rem;
+}
+</style>
