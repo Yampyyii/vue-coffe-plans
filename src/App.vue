5075aa4da1154c65ac4df6ccf0c188c38190d80e
Pallares <ing.pallares69@gmail.com>
2024-11-06 12:06:36 -0600

    LO ULTIMO😘Slots

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <script setup>
-import fancyButton from './components/FancyButton.vue';
+import planPicker from './components/plan-picker.vue';
+import{ref} from 'vue';
+const showPlans =ref(true);
 </script>
 
 <template>
-    <div class="content">
-      <fancy-button>
-        <template #icon="{ hover }">
-          {{ hover ? "🌟" : "⭐"}}
-        </template>
-        Hazme Clic
-      </fancy-button>
+  <header>
+    <div>
+      <span id="logo">Liquid Gold Box</span>
+      <img src="./assets/logo.png" alt="logo">
     </div>
-  </template>
+  </header>
+
+  <div class="content">
+    <h1 class="title">Coffee Plans</h1>
+
+    <h2 class="subtitle">
+      Viajamos por el mundo para encontrar el mejor café de origen único para ti
+    </h2>
+   <label>
+    <input type="checkbox" v-model="showPlans">
+    Mostrar Selector de Planes 
+   </label>
+    <plan-picker v-if="showPlans"/>
   
+  </div>
+</template>
+
 <style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
 </style>
